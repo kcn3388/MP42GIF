637b27f6599aa3d0e62ec12cc8d683548e8fc5f7
kcn3388 <kcn3388@126.com>
2024-12-26 23:08:58 +0800

    add new function, fix old bugs

--- convert_mp4.py
+++ convert_mp4.py
@@ -4,9 +4,9 @@ import copy
 import cv2
 import os
 import re
-from typing import List
+from typing import List, Tuple
 
-from PIL import Image
+from PIL import Image, ImageSequence
 
 parser = argparse.ArgumentParser(description="mp4转换gif")
 parser.add_argument("load_path", type=str, help="视频读取路径，可以是一个文件夹")
@@ -20,7 +20,7 @@ parser.add_argument(
     "--size",
     type=float,
     default=1,
-    help="视频缩放，格式为大于0的小数，默认为0.5"
+    help="视频缩放，格式为大于0的小数，默认为1"
 )
 parser.add_argument(
     "-R",
@@ -49,39 +49,78 @@ parser.add_argument(
     "-C", "--combine", action='store_true',
     help="是否拼装一个GIF，默认否，不可与拆分、不保存GIF同时使用"
 )
+parser.add_argument(
+    "-A", "--apng", action='store_true',
+    help="是否保存为APNG，默认否，不可与拆分、不保存GIF同时使用"
+)
+parser.add_argument(
+    "-IS", "--imagesize",
+    type=str,
+    default="",
+    help="按指定尺寸保存，会覆盖比例参数"
+)
+parser.add_argument(
+    "-DS", "--disposal",
+    type=int,
+    default=0,
+    help="指定保存格式，能修复透明gif叠加的问题，模式为2"
+)
 args = parser.parse_args()
 gif_frames: List[Image.Image] = []
 
 
-def convert_mp4_to_jpgs(input_file, split_path):
+def convert_mp4_to_jpgs(input_file, split_path, is_img=False):
     global gif_frames
-    # 先将mp4文件的所有帧读取出保存为图片
-    video_capture = cv2.VideoCapture(input_file)
-    still_reading, image = video_capture.read()
-    frame_count = 0
-    f_name = os.path.splitext(os.path.split(input_file)[-1])[0]
-    if args.split:
-        os.makedirs(f"{split_path}/{f_name}") if not os.path.exists(f"{split_path}/{f_name}") else 0
-    while still_reading:
-        img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGBA))
-        gif_frames.append(img)
+    if is_img:
+        im = Image.open(input_file)
+        frames = [frame.copy() for frame in ImageSequence.Iterator(im)]
+        gif_frames = frames
         if args.split:
-            img.save(f"{split_path}/{f_name}/{frame_count}.png", "PNG")
-        # read next image
+            frame_count = 0
+            f_name = os.path.splitext(os.path.split(input_file)[-1])[0]
+            os.makedirs(f"{split_path}/{f_name}") if not os.path.exists(f"{split_path}/{f_name}") else 0
+            for frame in gif_frames:
+                frame.save(f"{split_path}/{f_name}/000{frame_count}.png", "PNG")
+                frame_count += 1
+
+    else:
+        # 先将mp4文件的所有帧读取出保存为图片
+        video_capture = cv2.VideoCapture(input_file)
         still_reading, image = video_capture.read()
-        frame_count += 1
+        frame_count = 0
+        f_name = os.path.splitext(os.path.split(input_file)[-1])[0]
+        if args.split:
+            os.makedirs(f"{split_path}/{f_name}") if not os.path.exists(f"{split_path}/{f_name}") else 0
+        while still_reading:
+            img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGBA))
+            gif_frames.append(img)
+            if args.split:
+                img.save(f"{split_path}/{f_name}/000{frame_count}.png", "PNG")
+            # read next image
+            still_reading, image = video_capture.read()
+            frame_count += 1
 
 
-def convert_images_to_gif(output_file):
+def convert_images_to_gif(output_file, resize_ratio: Tuple[int, int] = None):
     # 读取目录下图片，用Pillow模块的Image和所有图片合并成一张gif
     global gif_frames
     frames_origin: List[Image.Image] = copy.deepcopy(gif_frames)
-    frames = [
-        frame.resize(
-            (round(frame.width * args.size), round(frame.height * args.size)),
-            Image.LANCZOS,
-        ) for frame in frames_origin
-    ]
+    if resize_ratio:
+        frames = [
+            frame.resize(
+                resize_ratio,
+                Image.Resampling.LANCZOS,
+            ) for frame in frames_origin
+        ]
+    elif not args.size == 1:
+        frames = [
+            frame.resize(
+                (round(frame.width * args.size), round(frame.height * args.size)),
+                Image.Resampling.LANCZOS,
+            ) for frame in frames_origin
+        ]
+    else:
+        frames = copy.deepcopy(frames_origin)
     # frames_rgba = []
     # for single_frame in frames:
     #     white_pixel = (255, 255, 255, 255)
@@ -95,23 +134,45 @@ def convert_images_to_gif(output_file):
     # final = frames_rgba if args.alpha else frames
     # frame_one = final[0]
     frame_one = frames[0]
-    frame_one.save(
-        f"{output_file}.gif",
-        format="GIF",
-        append_images=[
-            # f for i, f in enumerate(final[1:]) if i % args.reduce == 0
-            f for i, f in enumerate(frames[1:]) if i % args.reduce == 0
-        ],
-        save_all=True,
-        loop=0,
-        duration=args.duration,
-    )
+    if resize_ratio:
+        frame_one.save(
+            f"{output_file}.png" if args.apng else f"{output_file}.gif",
+            format="PNG" if args.apng else "GIF",
+            append_images=[
+                # f for i, f in enumerate(final[1:]) if i % args.reduce == 0
+                f for i, f in enumerate(frames[1:]) if i % args.reduce == 0
+            ],
+            save_all=True,
+            loop=0,
+            disposal=args.disposal
+        )
+    else:
+        frame_one.save(
+            f"{output_file}.png" if args.apng else f"{output_file}.gif",
+            format="PNG" if args.apng else "GIF",
+            append_images=[
+                # f for i, f in enumerate(final[1:]) if i % args.reduce == 0
+                f for i, f in enumerate(frames[1:]) if i % args.reduce == 0
+            ],
+            save_all=True,
+            loop=0,
+            duration=args.duration,
+            disposal=args.disposal
+        )
 
     gif_frames = []
 
 
 def convert_mp4_to_gif(load, save):
+    is_img = False
     global gif_frames
+    if args.imagesize:
+        try:
+            resize_ratio: Tuple[int, int] = tuple([int(x) for x in args.imagesize.split("*")])  # noqa
+        except Exception:
+            raise "尺寸输入错误，格式应为x*y"
+    else:
+        resize_ratio = None  # noqa
     if args.combine and args.nogif:
         raise "参数冲突"
     if args.combine and args.split:
@@ -137,9 +198,14 @@ def convert_mp4_to_gif(load, save):
                 file_path = os.path.join(os.getcwd(), load, image)
                 image_file_name = os.path.splitext(os.path.split(image)[-1])
                 if not re.match(r"\.mp4", image_file_name[-1], re.IGNORECASE):
-                    continue
+                    if re.match(r"(\.webp|\.gif)", image_file_name[-1], re.IGNORECASE):
+                        is_img = True
+                    else:
+                        continue
                 save_path = os.path.join(os.getcwd(), save, image_file_name[0])
         else:
+            if re.search(r"(\.webp|\.gif)", file_path, re.IGNORECASE):
+                is_img = True
             save_path = os.path.join(os.getcwd(), save)
         if args.combine:
             for each in images:
@@ -147,8 +213,8 @@ def convert_mp4_to_gif(load, save):
             convert_images_to_gif(save_path)
             break
         else:
-            convert_mp4_to_jpgs(file_path, save_path)
-            convert_images_to_gif(save_path) if not args.nogif else 0
+            convert_mp4_to_jpgs(file_path, save_path, is_img)
+            convert_images_to_gif(save_path, resize_ratio) if not args.nogif else 0
 
 
 convert_mp4_to_gif(args.load_path, args.save_path)
